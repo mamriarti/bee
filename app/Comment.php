b8e925c29f0dd80fc39967c1274f1a1d6f4a5a03
Mamrenko Alexandra <mamriarti@gmail.com>
2019-08-30 18:52:24 +0300

    связи между таблицами, метод Sluggable

---
+++ app/Comment.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    public function post(){
+    	return $this->hasOne(Post::class);
+    }
+
+    public function author(){
+    	return $this->hasOne(User::class);
+    }
+}
---
+++ app/Post.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
+class Post extends Model
+{
+    use Sluggable;
+
+    
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+    
+
+    public function category(){
+
+    	return $this->hasOne(Category::class);
+
+    }
+    public function author(){
+
+    	return $this->hasOne(User::class);
+    }
+    public function tags(){
+
+    	return $this->belongsToMany(
+
+    		Tag::class,
+    		'post_tags',
+    		'post_id',
+    		'tag_id'
+
+    		);
+    }
+
+}
