df0537880af63df2281215c4591d8c9f6b0dd50a
Mamrenko Alexandra <mamriarti@gmail.com>
2019-09-02 19:48:21 +0300

    add metod toogleStatus in model Comment

--- app/Comment.php
+++ app/Comment.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+   const IS_ALLOW = 1;
+   const IS_DISALLOW = 0;
+
+
     public function post(){
     	return $this->hasOne(Post::class);
     }
@@ -13,4 +17,26 @@ class Comment extends Model
     public function author(){
     	return $this->hasOne(User::class);
     }
+
+    public function allow(){
+    	$this->status = Comment::IS_ALLOW;
+    	$this->save();
+
+    }
+
+    public function disallow(){
+    	$this->status = Comment::IS_DISALLOW;
+    	$this->save();
+    }
+
+     public function toogleStatus(){
+     	if($this->status = Comment::IS_DISALLOW;){
+     		return $this->allow();
+     	}
+     	return $this->disallow();
+
+     }
+     public function remove(){
+     	$this->delete();
+     }
 }
