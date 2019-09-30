2e2d3abfad108fda70fbaa21f3413bea23343203
mamriarti <mamriarti@gmail.com>
2019-09-30 20:13:19 +0300

    create menu posts

--- app/Http/Controllers/Admin/PostsController.php
+++ app/Http/Controllers/Admin/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use  App\Post;
 
 class PostsController extends Controller
 {
@@ -14,7 +15,8 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        $posts = Post::all();
+        return view('admin.posts.index', compact('posts' ,$posts));
     }
 
     /**
