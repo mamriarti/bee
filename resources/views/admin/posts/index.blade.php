e65bdf13d83f18a26c535a123afbc2f16131d74d
Mamrenko Alexandra <mamriarti@gmail.com>
2019-10-01 20:16:28 +0300

    index litst posts

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,132 @@
+@extends('adminlte::page')
+
+@section('title', 'Dashboard')
+
+@section('content_header')
+    <h1>Посты</h1>
+@stop
+
+@section('content')
+
+    <section class="content">
+        <div class="row">
+            <div class="col-xs-12">
+
+                <div class="box">
+                    <div class="box-header">
+                        <h3 class="box-title">Посты</h3>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+
+                        <div class="form-group">
+                            <a href="{{route('posts.create')}}" class="btn btn-success">Добавить</a>
+                        </div>
+
+                        <table id="example1" class="table table-bordered table-striped">
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Название</th>
+                                <th>Категории</th>
+                                <th>Теги</th>
+                                <th>Картинка</th>
+                                <th>Редактировать</th>
+                                <th>Удалить</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            @foreach($posts as $post)
+                                <tr>
+                                    <td>{{$post->id}}</td>
+                                    <td>{{$post->title}}
+                                    </td>
+                                    <td>
+
+                                    </td>
+                                    <td>
+
+                                    </td>
+                                    <td>
+                                       <img src="{{$post->getImage()}}" alt="" width="100">
+                                    </td>
+                                    <td>
+                                        <a href="{{route('posts.edit', $post->id)}}"  class="fas fa-edit"></a>
+                                    </td>
+                                    <td>
+                                        {{ Form::open(['route' => ['posts.destroy', $post->id], 'method' => 'delete' ]) }}
+                                        <button onclick="return confirm('А вы уверены?')" type="submit" class="btn btn-danger">
+                                            <i class="fas fa-trash"></i>
+                                        </button>
+                                        {{ Form::close() }}
+                                    </td>
+
+
+                                </tr>
+                            @endforeach
+
+
+
+                            </tbody>
+                            <tfoot>
+                            <tr>
+                                <th>ID</th>
+                                <th>Название</th>
+                                <th>Категории</th>
+                                <th>Теги</th>
+                                <th>Картинка</th>
+                                <th>Редактировать</th>
+                                <th>Удалить</th>
+                            </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </section>
+
+
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    !-- jQuery 3 -->
+    <script src="../../bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="../../bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- DataTables -->
+    <script src="../../bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+    <script src="../../bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+    <!-- SlimScroll -->
+    <script src="../../bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <!-- FastClick -->
+    <script src="../../bower_components/fastclick/lib/fastclick.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../../dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../../dist/js/demo.js"></script>
+    <!-- page script -->
+    <script>
+        $(function () {
+            $('#example1').DataTable()
+            $('#example2').DataTable({
+                'paging'      : true,
+                'lengthChange': false,
+                'searching'   : false,
+                'ordering'    : true,
+                'info'        : true,
+                'autoWidth'   : false
+            })
+        })
+    </script>
+@stop
